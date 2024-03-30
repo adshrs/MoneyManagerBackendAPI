844c5e283d9d5fa11f8a8b34b5aa89a08127e529
Aditya C Shrestha <adityashrs@gmail.com>
2024-03-30 19:02:07 +0545

    Initial commit

---
+++ src/test/kotlin/com/example/moneymanagerbackend/MoneymanagerBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.moneymanagerbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoneymanagerBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
